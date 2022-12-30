3d2f097b9c8b84846f246a33b0d82343b7c92e0b
DxAmbar <d.ambarbaeza@gmail.com>
2022-12-30 13:47:09 -0300

    Evento onClick

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+
+    const handleAdd = (event) => {
+         console.log(event) 
+        }
+
     return (
     <>
     <h1>CounterApp</h1>
     <h2>{ value }</h2>
+
+    <button onClick={ handleAdd }>
+        +1
+    </button>
     </>
     )
 }
 
-CounterApp.protTypes
+CounterApp.protTypes = {
+    value: PropTypes.number.isRequired,
+}
