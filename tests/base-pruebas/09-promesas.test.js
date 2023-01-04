1d61449a80445db8407205f4658b3e8dc31a967c
DxAmbar <d.ambarbaeza@gmail.com>
2023-01-03 21:22:34 -0300

    Pruebas en 09, tareas asíncronas

---
+++ tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,40 @@
+import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
+
+
+describe('Pruebas en 09-promesas', () => { 
+
+    test('getHeroeByIdAsync debe retornar un héroe', (done) => { 
+
+        const id = 1;
+        getHeroeByIdAsync(id)
+            .then( hero => {
+
+                expect(hero).toEqual({
+                    id: 1,
+                    name: 'Batman',
+                    owner: 'DC'
+                },);
+
+                done();
+
+             });
+
+
+    });
+
+    test('getHeroeByIdAsync debe arrojar error si héroe no existe', (done) => { 
+
+        const id = 100;
+        getHeroeByIdAsync(id)
+            .catch(error =>{
+                
+                expect(error).toBe(`No se pudo encontrar el héroe con id ${id}`)
+                
+                done();
+            })
+
+
+    });
+
+
+});
