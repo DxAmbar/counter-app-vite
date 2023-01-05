7b2e1d41c7419740e467d09706f19cdadda7f1d6
DxAmbar <d.ambarbaeza@gmail.com>
2023-01-04 21:00:44 -0300

    Últimas pruebas en FirstApp

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -4,25 +4,47 @@ import { App } from "../src/FirstApp";
 
 describe('Pruebas en FirstApp', () => { 
 
-    test('Debe hacer match con el snapshot', () => {
+     /*test('Debe hacer match con el snapshot', () => {
 
         const title = 'Hola pirinola';
         const { container } = render( <App title={ title }/> );
 
         expect(container).toMatchSnapshot();
 
-    });
+    }); */
 
     
     test('Debe mostrar el título en un h1', () => { 
 
         const title = 'Hola pirinola';
-        const { container, getByText } = render( <App title={ title }/> );
+        const { container, getByText, getByTestId } = render( <App title={ title }/> );
 
         expect (getByText( title )).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
-        expect(h1.innerHTML).toContain( title );
+        /* const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain( title ); */
+
+        expect( getByTestId('test-title' ).innerHTML).toContain(title);
+
+    });
+
+    test('Debe de mostrar el subtítulo enviado por props', () => { 
+        
+        const title = 'Hola pirinola';
+        const subTitle = 'Soy un subtítulo';
+        const { getByText } = render( 
+            <App 
+                title={ title }
+                subTitle={ subTitle }
+            /> 
+        );
+
+
+        expect(getByText( subTitle )).toBeTruthy(); 
+        /*getAllByText para más de un elemento, por ejemplo para dos:
+        expect(getAllByText(subTitle).length).toBe(2);
+        ya que devuelve un arreglo con los números de elementos que encuentra y son dados*/
+
 
     });
 
---
+++ tests/FirstApp2.test.jsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { App } from "../src/FirstApp";
+
+
+describe('Pruebas en FirstApp', () => { 
+
+ 
+
+
+});
