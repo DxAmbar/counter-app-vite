1c9b2aa0e1b022a8b9f08b920650ff6bf10742ff
DxAmbar <d.ambarbaeza@gmail.com>
2023-01-05 10:23:14 -0300

    Simulando y probando evento click

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { CounterApp } from "../src/CounterApp"
 
 
 describe('Pruebas en el <CounterApp/>', () => { 
 
     const initialValue = 100;
+    const secondValue = 10;
 
     test('Debe de hacer match con el snapshot', () => { 
        
@@ -18,8 +19,37 @@ describe('Pruebas en el <CounterApp/>', () => {
         render(<CounterApp value={initialValue}/>); //o se puede poner directamente value={100}
         expect(screen.getByText(100)).toBeTruthy();
 
+    })
 
+    test('Debe de incrementar con el botón +1', () => {  
 
+        render(<CounterApp value={secondValue}/>);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('11')).toBeTruthy();
+
+    })
+
+    test('Debe de decrementar con el botón -1', () => {  
+
+        render(<CounterApp value={secondValue}/>);
+        fireEvent.click(screen.getByText('-1'));
+        screen.debug();
+        expect(screen.getByText('9')).toBeTruthy();
+
+    })
+
+    test('Debe funcionar el botón reset', () => {  
+
+        render(<CounterApp value={secondValue}/>);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        //fireEvent.click(screen.getByText('Reset'));
+        fireEvent.click(screen.getByRole('button', {name:'btn-reset'})) //también se puede buscar por id
+        screen.debug();
+
+        expect(screen.getByText(secondValue)).toBeTruthy();
 
     })
     
